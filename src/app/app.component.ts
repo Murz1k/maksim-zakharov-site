fd34181cd840a521ec8307883347289d65315bb8
Zakharov Maksim <Max89701@gmail.com>
2018-10-09 10:11:30 +0300

    Добавлена шапка и форма обратной связи

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  form: FormGroup;
+
+  budgets = [];
+  deadlines = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      budget: 0,
+      deadline: 1
+    });
+    this.budgets = [
+      '100 000 руб.',
+      '200 000 руб.',
+      '400 000 руб.',
+      '600 000 руб.',
+      '1 000 000 руб.',
+      '2 000 000 руб.',
+      '4 000 000 руб.',
+      '10 000 000 руб.'
+    ];
+
+    this.deadlines = [
+      'Срочно',
+      '1 месяц',
+      '2 месяц',
+      '3 месяц',
+      '4 месяц',
+      '5 месяц',
+      '6 месяц',
+      '7 месяц',
+      '8 месяц',
+      '9 месяц',
+      '10 месяц',
+      '11 месяц',
+      '12 месяц',
+      'Не знаю'
+    ];
+  }
+
+  get deadline(): string {
+    return this.deadlines[this.form.controls['deadline'].value];
+  }
+
+  get budget(): string {
+    return this.budgets[this.form.controls['budget'].value];
+  }
 }
