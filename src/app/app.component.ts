116d613e9a8885e20cd6753e0593ef7ecb11657d
Zakharov Maksim <Max89701@gmail.com>
2018-10-19 16:36:39 +0300

    Блокировка скролла

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterContentInit, Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
@@ -6,13 +6,27 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
   form: FormGroup;
 
   budgets = [];
   deadlines = [];
+  lastScrollTop = 0;
+
+  keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+  scrolling = false;
+
+  sectionHeight = 0;
+
+  load = false;
+
+  @ViewChild('main') public main: ElementRef;
+  @ViewChild('info') public info: ElementRef;
+  @ViewChild('why') public why: ElementRef;
+  @ViewChild('calculator') public calculator: ElementRef;
 
   constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
@@ -48,11 +62,68 @@ export class AppComponent {
     ];
   }
 
+  preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.returnValue = false;
+  }
+
+  preventDefaultForScrollKeys(e) {
+    if (this.keys[e.keyCode]) {
+      this.preventDefault(e);
+      return false;
+    }
+  }
+
+  disableScroll() {
+    if (window.addEventListener) { // older FF
+      window.addEventListener('DOMMouseScroll', this.preventDefault, false);
+    }
+    window.onwheel = this.preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = this.preventDefault; // older browsers, IE
+    window.ontouchmove = this.preventDefault; // mobile
+    document.onkeydown = this.preventDefaultForScrollKeys;
+  }
+
+  @HostListener('document:wheel', ['$event'])
+  private onWheel($event: WheelEvent): void {
+    console.log($event);
+    let height = 0;
+    if ($event.deltaY > 0) {// down
+      if (this.lastScrollTop === 0) {
+        height = this.sectionHeight * 0;
+      }
+    } else {// top
+      height = this.sectionHeight * 0;
+    }
+    window.scrollTo({left: 0, top: height, behavior: 'smooth'});
+    this.lastScrollTop = height;
+  }
+
+  @HostListener('document:scroll')
+  private onScroll(): void {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (!this.load && this.lastScrollTop === 0 && st !== this.lastScrollTop) {
+      window.scrollTo({left: 0, top: 0});
+      setTimeout(() => {
+        this.load = true;
+      }, 150);
+      return;
+    }
+  }
+
   get deadline(): string {
     return this.deadlines[this.form.controls['deadline'].value];
   }
 
   get budget(): string {
     return this.budgets[this.form.controls['budget'].value];
+  }
+
+  ngOnInit(): void {
+    this.disableScroll();
+    this.sectionHeight = this.main.nativeElement.offsetHeight;
   }
 }
