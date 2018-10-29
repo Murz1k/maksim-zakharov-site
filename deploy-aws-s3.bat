06ff762ad48491087eb8669754d357b2d2f96d90
Zakharov Maksim <Max89701@gmail.com>
2018-10-29 05:23:21 +0300

    Фикс скрипта и добавление опыта работы

--- deploy-aws-s3.bat
+++ deploy-aws-s3.bat
@@ -1,3 +1,4 @@
 SET var=%cd%
 cd C:\Program Files\Amazon\AWSCLI\bin\
-aws s3 sync "%var%"/dist/maksim-zakharov-site/ s3://maksim-zakharov.com --profile admin
+aws s3 sync "%var%"/dist/maksim-zakharov-site/ s3://maksim-zakharov.com --acl public-read --delete --profile admin
+exit
